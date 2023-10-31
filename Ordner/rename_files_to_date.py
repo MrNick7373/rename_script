5b723eed1f75fac94dab171ee6d9bc58eda95e1f
Robert Kniess <robert.kniess@iws.fraunhofer.de>
2023-10-31 14:05:44 +0100

    extract general methods to own file

--- Ordner/rename_files_to_date.py
+++ Ordner/rename_files_to_date.py
@@ -1,14 +1,6 @@
-import os, time, sys
+import time, sys
 from pathlib import Path
-
-def to_str(number: int) -> str:
-    if number < 10:
-        return f"0{number}"
-    else:
-        return f"{number}"
-    
-def create_path(path: Path, name: str, name_prefix: str = "") -> Path:
-    return Path(f"{path.parent}\\{name_prefix}{name}{path.suffix}")
+from rename_lib import *
 
 def create_name_from_modification_date(path: Path) -> str:
     date = time.gmtime(os.path.getmtime(path))
@@ -18,18 +10,6 @@ def create_name_from_modification_date(path: Path) -> str:
 
 def create_new_path(path: Path, name_prefix: str) -> Path:
     return create_path(path, create_name_from_modification_date(path), name_prefix)
-
-def get_files_from_directory(directory: Path = Path(__file__).parent) -> list[Path]:
-    return [Path(os.path.join(directory, file)) for file in os.listdir(directory) if os.path.isfile(os.path.join(directory, file))]
-
-def rename_file(old_path: Path, new_path: Path):
-    if old_path == Path(__file__):
-        return
-    if new_path.exists():
-        print(f"skipped >{old_path.stem}{old_path.suffix}<")
-    else:
-        print(f"renamed >{old_path.stem}{old_path.suffix}< to >{new_path.stem}{new_path.suffix}<")
-        old_path.rename(new_path)
 
 def rename_files_to_date(path_list: list[Path], name_prefix: str):
     for path in path_list:
---
+++ Ordner/rename_lib.py
@@ -0,0 +1,24 @@
+import os
+from pathlib import Path
+
+def to_str(number: int) -> str:
+    if number < 10:
+        return f"0{number}"
+    else:
+        return f"{number}"
+    
+def create_path(path: Path, name: str, name_prefix: str = "") -> Path:
+    return Path(f"{path.parent}\\{name_prefix}{name}{path.suffix}")
+
+def get_files_from_directory(directory: Path = Path(__file__).parent) -> list[Path]:
+    return [Path(os.path.join(directory, file)) for file in os.listdir(directory) if os.path.isfile(os.path.join(directory, file))]
+
+def rename_file(old_path: Path, new_path: Path):
+    if old_path == Path(__file__):
+        return
+    if new_path.exists():
+        print(f"skipped >{old_path.stem}{old_path.suffix}<")
+    else:
+        print(f"renamed >{old_path.stem}{old_path.suffix}< to >{new_path.stem}{new_path.suffix}<")
+        old_path.rename(new_path)
+        
