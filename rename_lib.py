a15dba92973ada99f11ae33f90a96a8c3c5ce64e
haldircane <haldircane@gmail.com>
2023-10-31 17:50:02 +0100

    index (from ascending) only increases if file was not skipped

--- rename_lib.py
+++ rename_lib.py
@@ -21,13 +21,14 @@ def create_path(path: Path, name: str, name_prefix: str = "") -> Path:
 def get_files_from_directory(directory: Path = Path(__file__).parent) -> list[Path]:
     return [Path(os.path.join(directory, file)) for file in os.listdir(directory) if os.path.isfile(os.path.join(directory, file))]
 
-def rename_file(old_path: Path, new_path: Path):
+def rename_file(old_path: Path, new_path: Path) -> bool:
     skip_suffix: list[str] = [".py", ".md", ".gitignore"]
     skip_name: list[str] = [".gitignore"]
     if skip_suffix.count(old_path.suffix) != 0 or skip_name.count(old_path.name) != 0:
-        return
+        return False
     if new_path.exists():
         print(f"skipped >{old_path.stem}{old_path.suffix}<")
     else:
         print(f"renamed >{old_path.stem}{old_path.suffix}< to >{new_path.stem}{new_path.suffix}<")
         old_path.rename(new_path)
+    return True
