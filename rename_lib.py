cbb4dc388aebd1889c96197e755c1dcf158ddd45
haldircane <haldircane@gmail.com>
2023-10-31 17:47:12 +0100

    add skip lists to rename_file

--- rename_lib.py
+++ rename_lib.py
@@ -22,7 +22,9 @@ def get_files_from_directory(directory: Path = Path(__file__).parent) -> list[Pa
     return [Path(os.path.join(directory, file)) for file in os.listdir(directory) if os.path.isfile(os.path.join(directory, file))]
 
 def rename_file(old_path: Path, new_path: Path):
-    if old_path.suffix == ".py":
+    skip_suffix: list[str] = [".py", ".md", ".gitignore"]
+    skip_name: list[str] = [".gitignore"]
+    if skip_suffix.count(old_path.suffix) != 0 or skip_name.count(old_path.name) != 0:
         return
     if new_path.exists():
         print(f"skipped >{old_path.stem}{old_path.suffix}<")
