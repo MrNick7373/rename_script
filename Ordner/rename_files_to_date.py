ed861ffb92b923e2436a54332702986d715cdafb
Robert Kniess <robert.kniess@iws.fraunhofer.de>
2023-10-31 13:37:02 +0100

    extract create_path to a method

--- Ordner/rename_files_to_date.py
+++ Ordner/rename_files_to_date.py
@@ -7,11 +7,17 @@ def to_str(number: int) -> str:
     else:
         return f"{number}"
     
-def create_name_from_creation_date(path: Path, name_prefix: str) -> str:
-    date = time.gmtime(os.path.getctime(path))
+def create_path(path: Path, name: str, name_prefix: str = "") -> Path:
+    return Path(f"{path.parent}\\{name_prefix}{name}{path.suffix}")
+
+def create_name_from_modification_date(path: Path) -> str:
+    date = time.gmtime(os.path.getmtime(path))
     date_str = f"{date.tm_year}{to_str(date.tm_mon)}{to_str(date.tm_mday)}"
     time_str = f"{to_str(date.tm_hour)}{to_str(date.tm_min)}{to_str(date.tm_sec)}"
-    return f"{path.parent}\\{name_prefix}{date_str}_{time_str}{path.suffix}"
+    return f"{date_str}_{time_str}"
+
+def create_new_path(path: Path, name_prefix: str) -> Path:
+    return create_path(path, create_name_from_modification_date(path), name_prefix)
 
 def get_files_from_directory(directory: Path = Path(__file__).parent) -> list[Path]:
     return [Path(os.path.join(directory, file)) for file in os.listdir(directory) if os.path.isfile(os.path.join(directory, file))]
@@ -27,7 +33,7 @@ def rename_file(old_path: Path, new_path: Path):
 
 def rename_files_to_date(path_list: list[Path], name_prefix: str):
     for path in path_list:
-        rename_file(path, Path(create_name_from_creation_date(path, name_prefix)))
+        rename_file(path, create_new_path(path, name_prefix))
 
 if __name__ == "__main__":
     args = sys.argv[1:]
