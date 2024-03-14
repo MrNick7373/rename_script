274f406f400e88676be7b54857e703d16b18c381
Robert Kniess <robert.kniess@iws.fraunhofer.de>
2024-03-14 18:45:08 +0100

    refactor len

--- equalise_file_name_length.py
+++ equalise_file_name_length.py
@@ -2,27 +2,27 @@ import sys
 from rename_lib import *
 
 def equalise_date(date_str: str, digits: int) -> str:
-    while date_str.__len__() < digits:
+    while len(date_str) < digits:
         date_str += "0"
-    while date_str.__len__() > digits:
+    while len(date_str) > digits:
         date_str = date_str[:-1]
     return date_str
 
-def create_new_path(path: Path, default_prefix: str, default_digits: int) -> Path:
+def create_new_path(path: Path, default_prefix: str, digits: int) -> Path:
     stem_list = path.stem.split("_")
-    new_date = equalise_date(stem_list[stem_list.__len__() - 1], default_digits)
+    new_date = equalise_date(stem_list[len(stem_list) - 1], digits)
     index: int = 0
     for entry in stem_list:
-        if index == stem_list.__len__() - 1:
+        if index == len(stem_list) - 1:
             break
         default_prefix += f"{entry}_"
         index += 1
     default_prefix += new_date
     return create_path(path, default_prefix)
 
-def equalise_file_name_length(path_list: list[Path], default_prefix: str, default_digits: int):
+def equalise_file_name_length(path_list: list[Path], default_prefix: str, digits: int):
     for path in path_list:
-        rename_file(path, create_new_path(path, default_prefix, default_digits))
+        rename_file(path, create_new_path(path, default_prefix, digits))
 
 if __name__ == "__main__":
     args = sys.argv[1:]
