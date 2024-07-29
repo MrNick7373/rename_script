e9a795d7405b3ede9efc63231fafa4d4ec81c3da
Robert Kniess <robert.kniess@iws.fraunhofer.de>
2024-07-29 16:22:52 +0200

    refactor

--- rename_lib.py
+++ rename_lib.py
@@ -20,8 +20,7 @@ def is_valid_file(path: Path) -> bool:
 def get_files_from_directory(directory: Path = Path(__file__).parent) -> list[Path]:
     #return [Path(os.path.join(directory, file)) for file in os.listdir(directory) if (os.path.isfile(os.path.join(directory, file)) and is_valid_file(Path(os.path.join(directory, file))))]
     paths: list[Path] = []
-    for file in directory.iterdir():
-        path: Path = directory / file
+    for path in directory.iterdir():
         if path.is_file() and is_valid_file(path):
             paths.append(path)
     return paths
